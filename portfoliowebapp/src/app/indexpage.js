299c5147eceaab989434a708f5474424c546c6c6
Maxime <maximedelange1991@hotmail.com>
2017-10-17 17:05:15 +0200

    Video playing functioning on Android and IOS, Chrome and Safari for mobile

--- portfoliowebapp/src/app/indexpage.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("app", []);
-
-app.controller("videoplayer", function($scope){
-  $scope.send = function(){
-    var video = document.getElementById('video');
-    video.play();
-  }
-})
