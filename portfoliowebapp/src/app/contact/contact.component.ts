0b81799b6b8845e115a469528c7e7bfbe78a98db
Maxime <maximedelange1991@hotmail.com>
2017-10-22 11:40:08 +0200

    Completed contact page layout with routing

--- portfoliowebapp/src/app/contact/contact.component.ts
+++ portfoliowebapp/src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { trigger, state, style, transition, animate, keyframes, query, stagger } from '@angular/animations'
+import {Router} from "@angular/router";
 
 @Component({
   templateUrl: './contact.component.html',
@@ -11,7 +12,7 @@ import { trigger, state, style, transition, animate, keyframes, query, stagger }
         query('ul', style({
           transform: 'translateX(-100%)'
         })),
-        query('ul', stagger('600ms', [
+        query('ul', stagger('1200ms', [
           animate('900ms', style({transform: 'translateX(0)'}))
         ]))
       ])
@@ -22,10 +23,17 @@ import { trigger, state, style, transition, animate, keyframes, query, stagger }
 export class ContactComponent {
   // Fields
   title = 'Contact';
-  contacts = ['maxime.de.lange@outlook.com', 'facebook/maxime', 'linkedin/maxime', 'github/maxime'];
+  contacts = [
+    'E-MAIL',
+    'FACEBOOK',
+    'LINKEDIN',
+    'GITHUB'
+  ];
   show = false;
+  router: Router;
 
-  constructor(){
+  constructor(router: Router){
+    this.router = router;
   }
 
   get stateName(){
@@ -34,5 +42,22 @@ export class ContactComponent {
 
   toggle(){
     this.show = !this.show;
+  }
+
+  getHyperLinks(hyperLinkName){
+    switch (hyperLinkName){
+      case 'E-MAIL':
+        this.contacts[0] = 'MAXIME.DE.LANGE@OUTLOOK.COM';
+        break;
+      case 'FACEBOOK':
+        window.location.href = 'https://www.facebook.com/maxime.delange';
+        break;
+      case 'LINKEDIN':
+        window.location.href = 'https://www.linkedin.com/in/maxime-de-lange-867a4165/';
+        break;
+      case 'GITHUB':
+        window.location.href = 'https://github.com/felucius';
+        break;
+    }
   }
 }
