255b653b68b6bf73e4a42ee20d2645ff7896e521
Maxime <maximedelange1991@hotmail.com>
2017-10-23 16:37:43 +0200

    Improved styling and animations for page skills

---
+++ portfoliowebapp/src/app/skill/skill-object.component.ts
@@ -0,0 +1,6 @@
+export class Skill{
+  id: number;
+  name: String;
+  image: String;
+  framework: String;
+}
--- portfoliowebapp/src/app/skill/skill.component.ts
+++ portfoliowebapp/src/app/skill/skill.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import {Skill} from "./skill-object.component";
+import {trigger, transition, query, style, stagger, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './skill.component.html'
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.css'],
+
+  animations: [
+    trigger('skillAnimation', [
+      transition('* => *', [
+        query('ul', style({
+          transform: 'translateX(-100%)'
+        })),
+        query('ul', stagger('600ms', [
+          animate('900ms', style({transform: 'translateX(0)'}))
+        ]))
+      ])
+    ])
+  ]
 })
 
 export class SkillComponent {
-  title = 'Skills'
+  // Fields
+  title = 'Skills';
+  show = false;
+  skills: Skill[] = [
+    {id: 1, name: 'JAVA', image: 'assets/images/iteration2/contact.png', framework: '+ WEB FRAMEWORKS'},
+    {id: 2, name: 'C#', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 3, name: 'HTML', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 4, name: 'CSS', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 5, name: 'JAVASCRIPT', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 6, name: 'BOOTSTRAP', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 7, name: 'TYPE SCRIPT', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 8, name: 'SQL', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 9, name: 'MYSQL', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 10, name: 'ANDROID', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 11, name: 'IOS', image: 'assets/images/iteration2/contact.png', framework: null},
+    {id: 12, name: 'GIT', image: 'assets/images/iteration2/contact.png', framework: null},
+  ];
+
+  constructor(){
+  }
+
+  get stateName(){
+    return this.show ? 'show' : 'hide';
+  }
+
+  toggle(){
+    this.show = !this.show;
+  }
 }
