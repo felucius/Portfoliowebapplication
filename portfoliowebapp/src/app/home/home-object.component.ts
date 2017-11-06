cc2775fc9251dec5cfe4b6f68e963850c20222d1
Maxime <maximedelange1991@hotmail.com>
2017-11-06 13:08:10 +0100

    Huge change on layout and introduction page. Changed colors and made introduction page simpler and more clear

---
+++ portfoliowebapp/src/app/home/home-object.component.ts
@@ -0,0 +1,10 @@
+export class Home{
+  // Fields
+  id: number;
+  description: String;
+
+  constructor(id: number, description: String){
+    this.id = id;
+    this.description = description;
+  }
+}
--- portfoliowebapp/src/app/home/home.component.ts
+++ portfoliowebapp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {Home} from "./home-object.component";
 
 @Component({
   templateUrl: './home.component.html',
@@ -6,9 +7,22 @@ import {Component} from "@angular/core";
 })
 
 export class HomeComponent{
-
+  // Fields
+  name = 'Maxime de Lange';
+  joke = 'public void sayName(name)';
+  joke2 = 'public void showInterests(interest)';
+  ambitions: Home[];
   introduction ='Junior programmer with a passion for programming and designing.';
   constructor(){
+    this.createAmbition();
+  }
 
+  public createAmbition(){
+    this.ambitions = [
+      {id: 1, description: 'export class MaximeDeLange(){'},
+      {id: 2, description: 'ngOnInit(){'},
+      {id: 3, description: 'this.introduce()}'},
+      {id: 4, description: '};'}
+    ]
   }
 }
