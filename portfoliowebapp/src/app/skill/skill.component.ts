40ba12b494aa3920e9f193fddf1b87469c3db9e6
Maxime <maximedelange1991@hotmail.com>
2017-11-08 20:55:21 +0100

    Re-styled skill page. Card design is more vivid and interesting.

--- portfoliowebapp/src/app/skill/skill.component.ts
+++ portfoliowebapp/src/app/skill/skill.component.ts
@@ -33,20 +33,20 @@ export class SkillComponent {
 
   public createSkills(){
     this.skills = [
-      {id: 1, name: 'JAVA', image: 'assets/images/iteration2/logo/javalogo.png', framework: null},
-      {id: 2, name: 'C#', image: 'assets/images/iteration2/logo/csharplogo.jpg', framework: null},
-      {id: 3, name: 'HTML', image: 'assets/images/iteration2/logo/htmllogo.png', framework: null},
-      {id: 4, name: 'CSS', image: 'assets/images/iteration2/logo/csslogo.png', framework: null},
-      {id: 5, name: 'JAVASCRIPT', image: 'assets/images/iteration2/logo/javascriptlogo.png', framework: null},
-      {id: 6, name: 'BOOTSTRAP', image: 'assets/images/iteration2/logo/bootstraplogo.jpg', framework: null},
-      {id: 7, name: 'TYPE SCRIPT', image: 'assets/images/iteration2/logo/typescriptlogo.png', framework: null},
-      {id: 8, name: 'SQL', image: 'assets/images/iteration2/logo/sqllogo.png', framework: null},
-      {id: 9, name: 'MYSQL', image: 'assets/images/iteration2/logo/mysqllogo2.png', framework: null},
-      {id: 10, name: 'ANDROID', image: 'assets/images/iteration2/logo/androidlogo.jpg', framework: null},
-      {id: 11, name: 'IOS', image: 'assets/images/iteration2/logo/ioslogo.png', framework: null},
-      {id: 12, name: 'GIT', image: 'assets/images/iteration2/logo/gitlogo2.png', framework: null},
-      {id: 13, name: 'ANGULAR', image: 'assets/images/iteration2/logo/angular2logo.jpeg', framework: null},
-      {id: 14, name: 'UML', image: 'assets/images/iteration2/logo/umllogo.png', framework: null}
+      {id: 1, name: 'JAVA', image: 'assets/images/iteration3/skill/javalogo.png', framework: null},
+      {id: 2, name: 'C#', image: 'assets/images/iteration3/skill/csharplogo.png', framework: null},
+      {id: 3, name: 'HTML', image: 'assets/images/iteration3/skill/htmllogo.jpg', framework: null},
+      {id: 4, name: 'CSS', image: 'assets/images/iteration3/skill/csslogo.png', framework: null},
+      {id: 5, name: 'JAVASCRIPT', image: 'assets/images/iteration3/skill/javascriptlogo.png', framework: null},
+      {id: 6, name: 'BOOTSTRAP', image: 'assets/images/iteration3/skill/bootstraplogo.png', framework: null},
+      {id: 7, name: 'TYPE SCRIPT', image: 'assets/images/iteration3/skill/typescriptlogo.png', framework: null},
+      {id: 8, name: 'SQL', image: 'assets/images/iteration3/skill/sqllogo.png', framework: null},
+      {id: 9, name: 'MYSQL', image: 'assets/images/iteration3/skill/mysqllogo.png', framework: null},
+      {id: 10, name: 'ANDROID', image: 'assets/images/iteration3/skill/androidlogo.png', framework: null},
+      {id: 11, name: 'SWIFT', image: 'assets/images/iteration3/skill/swiftlogo.png', framework: null},
+      {id: 12, name: 'GIT', image: 'assets/images/iteration3/skill/gitlogo.png', framework: null},
+      {id: 13, name: 'ANGULAR', image: 'assets/images/iteration3/skill/angularlogo.png', framework: null},
+      {id: 14, name: 'UML', image: 'assets/images/iteration3/skill/umllogo.png', framework: null}
     ];
   }
 
